ce62b7814e3deb52453258bad5185c3b79a0a6ad
hyunyoung158 <setarong158@gmail.com>
2014-01-22 11:27:09 +0900

    버튼 애니메이션 딜레이 작성중입니다.

--- MemoryTest/ViewController.m
+++ MemoryTest/ViewController.m
@@ -18,13 +18,62 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    NSMutableArray *_randomList;
+    NSInteger stageNum;
+}
 
+- (IBAction)touchButton:(id)sender {
+//    UIButton *button = (UIButton *)sender;
+//    button.backgroundColor = [UIColor grayColor];
+}
+
+- (IBAction)selectButton:(id)sender {
+    UIButton *button = (UIButton *)sender;
+    UIColor *originalColor = button.backgroundColor;
+    
+    // 버튼 색 변경
+//    [UIView animateWithDuration:0.5 animations:^{
+//        button.backgroundColor = [UIColor grayColor];
+//    } completion:^(BOOL finished) {
+//        [UIView animateWithDuration:0.5 animations:^{
+//            button.backgroundColor = originalColor;
+//        }];
+//    }];
+    [self animationSelectButton:[NSNumber numberWithInteger:button.tag]];
+    
+    [self userInput:button.tag];
+}
 
 - (void)startStage {
     // 스테이지 시작 시 먼저 보여주는 과정~
     // 랜덤 배열을 생성해서 저장.
     // 배열에서 꺼내면서 버튼에 색깔로 보여주기.
+    // 유저 인풋 금지.
+    
+    [_randomList addObject:@(arc4random() % 4)];
+
+    
+    for (int i = 0; i < [_randomList count]; i++) {
+        NSInteger tagNum = [[_randomList objectAtIndex:i] integerValue];
+        [self performSelector:@selector(animationSelectButton:) withObject:[NSNumber numberWithInteger:tagNum] afterDelay:1.0];
+//        [self animationSelectButton:tagNum];
+    }
+}
+
+- (void)animationSelectButton:(NSNumber *) buttonTagNum {
+    NSLog(@"button tag num : %@", buttonTagNum);
+    UIButton *button = (UIButton *)[self.view viewWithTag:(NSInteger)buttonTagNum];
+    UIColor *originalColor = button.backgroundColor;
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        button.backgroundColor = [UIColor grayColor];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.5 animations:^{
+            button.backgroundColor = originalColor;
+        }];
+    }];
+    
 }
 
 - (void)userInput:(NSInteger) buttonNum {
@@ -32,10 +81,19 @@
     // 배열과 비교해서 맞으면 가만있고 틀리면 alertview 띄우고 끗.
 }
 
+// 스테이지 초기화
+- (void)stageInit {
+    _randomList = [[NSMutableArray alloc] init];
+    stageNum = 1;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
+    // 초기화
+    [self stageInit];
+    [self startStage];
 }
 
 - (void)didReceiveMemoryWarning
